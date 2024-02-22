db003b666c4d958c60c770eb63c366a5ea22be96
jes14 <yujin.kim@chainstep.com>
2024-02-22 12:08:45 +0100

    feat: generate plugin

---
+++ projects/plugin/src/lib/component-name/component-name.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'kd-component-name',
+  templateUrl: './component-name.component.html',
+  styleUrls: ['./component-name.component.scss']
+})
+export class ComponentNameComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- projects/plugin/src/lib/plugin.module.ts
+++ projects/plugin/src/lib/plugin.module.ts
@@ -1,10 +1,12 @@
 import {NgModule} from "@angular/core";
 import {CommonModule} from "@angular/common";
 import {K8sApiClientService} from "./k8s-api-client.service";
+import { ComponentNameComponent } from './component-name/component-name.component';
 
 @NgModule({
   imports: [CommonModule],
   providers: [K8sApiClientService],
+  declarations: [ComponentNameComponent],
 })
 export class PluginModule {
 }
